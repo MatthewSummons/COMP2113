97e66b42b045454730ecbf2d2c365e48440ed01c
Matthew <46129554+MatthewSummons@users.noreply.github.com>
2022-03-21 22:13:21 +0800

    Submitted Assignment 1

--- Assignment 1/Q4/test.sh
+++ Assignment 1/Q4/test.sh
@@ -2,7 +2,18 @@
 
 g++ -pedantic-errors -std=c++11 4.cpp -o 4
 
+touch output.txt
+
 for i in {1..1000}
 do
-    echo "$i" | ./4
+     echo $i "Loading ..."
+     echo "$i" | ./4 >> output.txt
 done
+
+touch nums.txt
+grep -n "flush" output.txt > nums.txt
+cut -d":" -f1 nums.txt > nums.txt
+cat nums.txt
+
+rm nums.txt
+rm output.txt
